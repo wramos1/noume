9ee277ed87268cbc3aefa1a51cb9431f349bcb57
Willyam Ramos <ramoswillyam1@gmail.com>
2023-02-11 23:48:47 -0800

    Implemented function to detect scrolling motion, going to modify it to detect when user is scrolling only on homepage in ordet to make navbar stick

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
+import { useState } from 'react';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
 const Navbar = (props) => {
     const location = useLocation();
+    const [scrollDir, setScrollDir] = useState("scrolling down");
+
+    useEffect(() => {
+        const threshold = 0;
+        let lastScrollY = window.pageYOffset;
+        let ticking = false;
+
+        const updateScrollDir = () => {
+            const scrollY = window.pageYOffset;
+
+            if (Math.abs(scrollY - lastScrollY) < threshold) {
+                ticking = false;
+                return;
+            }
+            setScrollDir(scrollY > lastScrollY ? "scrolling down" : "scrolling up");
+            lastScrollY = scrollY > 0 ? scrollY : 0;
+            ticking = false;
+        };
+
+        const onScroll = () => {
+            if (!ticking) {
+                window.requestAnimationFrame(updateScrollDir);
+                ticking = true;
+            }
+        };
+
+        window.addEventListener("scroll", onScroll);
+        console.log(scrollDir);
+
+        return () => window.removeEventListener("scroll", onScroll);
+    }, [scrollDir]);
     return (
-        <nav className={`flex justify-around items-center absolute z-20 w-full p-9 transition-all ${props.bg} ${props.paddingSize}`}>
+        <nav className={`nav flex justify-around items-center absolute z-20 w-full p-9 transition-all ${props.bg} ${props.paddingSize}`}>
             <div className='w-1/2'>
                 <h1 className='text-3xl secondary-txt-color'>
                     <Link to={'/'}>
